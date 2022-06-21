6fbe0e0c0479e98ad15909266e246623355567b9
DenisMiso4enko <denrrmisochenko17@gmail.com>
2022-06-22 01:05:28 +0300

    add burger menu

--- js/scroll.js
+++ js/scroll.js
@@ -6,3 +6,17 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         })
     })
 })
+
+// Ищем нужные элементы бургер и меню
+const burger = document.querySelector('.burger')
+const menu = document.querySelector('.menu__list')
+// Вещаем событие на бургер и добовляем переключатель с классом active
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active')
+    menu.classList.toggle('active')
+})
+// При нажатии на сылку из меню удаляем класс active
+document.querySelectorAll('.menu__list').forEach(n => n.addEventListener('click', () => {
+    burger.classList.remove('active')
+    menu.classList.remove('active')
+}))
