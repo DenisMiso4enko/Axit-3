505c0e6054979793023a9aecb8bcb9aee9cf6d81
DenisMiso4enko <denrrmisochenko17@gmail.com>
2022-06-22 18:56:43 +0300

    add mobile

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+/* ------------Табы----------- */
 const tabHeaders = document.querySelectorAll('[data-tab]')
 //console.log(headers);
 
@@ -31,3 +32,36 @@ tabHeaders.forEach(function(item) {
     })
 })
 
+/* ------------Скрол к секции по сылке----------- */
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault()
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior : "smooth"
+        })
+    })
+})
+
+/* ------------Меню бургер ----------- */
+
+// Ищем нужные элементы бургер и меню
+const burger = document.querySelector('.burger')
+const menu = document.querySelector('.menu__list')
+// Вещаем событие на бургер и добовляем переключатель с классом active
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active')
+    menu.classList.toggle('active')
+})
+// При нажатии на сылку из меню удаляем класс active
+document.querySelectorAll('.menu__list').forEach(n => n.addEventListener('click', () => {
+    burger.classList.remove('active')
+    menu.classList.remove('active')
+}))
+
+
+// slider 
+
+/* $('.reviews__card_inner').slick({
+    dots: true,
+    arrows: false,
+}); */
